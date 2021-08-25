bc7ff3ca41e8055c519a523fdfc7b5ddc8d0743f
lucasmetron <lucasmetron@gmail.com>
2021-08-25 15:21:59 -0300

    transition

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import './App.css';
+import Transition from 'react-transition-group/Transition'
 
 function App() {
 
@@ -11,7 +12,19 @@ function App() {
 
   return (
     <div>
-      <button onClick={handleClick} className={'btn ' + (isOn == true ? 'ligado' : 'desligado')}> Click</button>
+      <Transition appear={true} unmountOnExit={true} in={isOn} timeout={{
+        enter: 300,
+        exit: 500
+      }} >
+        {
+          (status) => {
+            return <button onClick={handleClick} className={'btn ' + status}> Click </button>
+          }
+        }
+
+      </Transition>
+
+
     </div>
   );
 }
