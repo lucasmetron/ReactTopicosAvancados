429bd6e2adb1ab18b1b78df9e268397a8b07f24a
lucasmetron <lucasmetron@gmail.com>
2021-08-25 14:14:28 -0300

    animação sem transition group

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const [isOn, setIson] = useState(true)
+
+  function handleClick() {
+    setIson(!isOn)
+  }
+
   return (
-    <div className="App">
-      teste
+    <div>
+      <button onClick={handleClick} className={'btn ' + (isOn == true ? 'ligado' : 'desligado')}> Click</button>
     </div>
   );
 }
