741c7b1d8140959d93fb557d5f36fc7a0a5876b8
lucasmetron <lucasmetron@gmail.com>
2021-08-26 16:03:21 -0300

    switch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import About from './views/About';
 import Index from './views/Index';
+import Adress from './views/about/Adress';
 
 function App() {
 
@@ -10,14 +11,18 @@ function App() {
   return (
     <div>
       <Router>
+
         <nav>
           <ul>
             <li> <Link to='/'> Index</Link> </li>
             <li> <Link to='/about/'>About</Link> </li>
           </ul>
         </nav>
-        <Route path='/' exact component={Index} />
-        <Route path='/about/' component={About} />
+        <Switch>
+          <Route path='/' exact component={Index} />
+          <Route path='/about/' component={About} />{/*Rota fixa pode vir antes de rotas com variaveis*/}
+          <Route path='/:abc' component={Adress} /> {/*Rota tiver várivel ela deve sempre ficar por ultimo*/}
+        </Switch>
       </Router>
 
     </div>
