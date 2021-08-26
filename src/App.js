66821d77c280d73158538fce341c0f976c02803e
lucasmetron <lucasmetron@gmail.com>
2021-08-26 15:18:31 -0300

    prompt

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Prompt } from 'react-router-dom';
 import About from './views/About';
 import Index from './views/Index';
 
@@ -10,15 +10,15 @@ function App() {
   return (
     <div>
       <Router>
+        <Prompt when={true} message={(location) => `Deseja sair para ${location.pathname}`} /> {/*Basta add o comp Prompt com esses 2 atributos */}
         <nav>
           <ul>
-            <li> <Link to='/about/abc'> Index</Link> </li>
+            <li> <Link to='/'> Index</Link> </li>
             <li> <Link to='/about/def'>About</Link> </li>
           </ul>
         </nav>
         <Route path='/' exact component={Index} />
-        <Route path='/about/:minhaVariavel' component={About} /> {/* : indica qé uma variavel que esta acessivel */}
-        <Route path='/about/:minhaVariavel(abc|def)' component={About} /> {/* () permite que somente rotas com valor abc e def acessam essa rota */}
+        <Route path='/about/:minhaVariavel' component={About} />
       </Router>
 
     </div>
