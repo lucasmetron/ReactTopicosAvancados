398a7aa98fb1f3fb61a669bd49f9822ea54fa6dc
lucasmetron <lucasmetron@gmail.com>
2021-08-26 16:22:15 -0300

    hooks router

--- src/views/about/Contact.js
+++ src/views/about/Contact.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { useHistory, useLocation, useParams, useRouteMatch } from 'react-router-dom';
 
 export default function Contact(props) {
+
+    const history = useHistory(); //faz redirecionamento de página
+    const location = useLocation(); //Mosta a url 
+    const params = useParams(); //pega parametros da URL
+    const match = useRouteMatch('');// verifica se uma rota bate com a rota atual
+
+    setTimeout(() => {
+        history.push('/')
+    }, 2000)
+    console.log(history)
 
 
     return (
